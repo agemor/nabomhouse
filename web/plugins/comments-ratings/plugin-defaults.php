2266507e5c208527f726f563cef800e91afcab37
HyunJun Kim <me@hyunjun.org>
2016-08-02 14:38:27 +0900

    Add Korean translations to comment plugin

--- web/plugins/comments-ratings/plugin-defaults.php
+++ web/plugins/comments-ratings/plugin-defaults.php
@@ -6,11 +6,11 @@
 	'enable_selective_ratings' => false,
 	'default_rating'           => 4,
 	'display_on_post_types'    => array( 'post' => 'on', 'page' => 'on' ),
-	'review_rating_label'      => esc_html__( 'Your overall rating of this listing:', 'comments-ratings' ),
-	'review_title_label'       => esc_html__( 'Title of your review:', 'comments-ratings' ),
-	'review_title_placeholder' => esc_html__( 'Summarize your opinion or highlight an interesting detail', 'comments-ratings' ),
-	'review_label'             => esc_html__( 'Your Review', 'comments-ratings' ),
-	'review_placeholder'       => esc_html__( 'Tell about your experience or leave a tip for others', 'comments-ratings' ),
-	'review_submit_button'     => esc_html__( 'Submit your Review', 'comments-ratings' ),
+	'review_rating_label'      => esc_html__( '이 하숙에 대한 여러분의 평가점수:', 'comments-ratings' ),
+	'review_title_label'       => esc_html__( '리뷰 제목:', 'comments-ratings' ),
+	'review_title_placeholder' => esc_html__( '간단하게 리뷰의 내용을 요약해 주세요', 'comments-ratings' ),
+	'review_label'             => esc_html__( '상세 리뷰', 'comments-ratings' ),
+	'review_placeholder'       => esc_html__( '이 하숙에 대한 여러분의 전반적인 평가를 요약해 주세요', 'comments-ratings' ),
+	'review_submit_button'     => esc_html__( '리뷰 올리기', 'comments-ratings' ),
 
 ); # config
--- web/plugins/comments-ratings/settings/labels.php
+++ web/plugins/comments-ratings/settings/labels.php
@@ -6,37 +6,37 @@ $labels_settings = array(
 	'options' => array(
 		'review_rating_label'      => array(
 			'label'   => esc_html__( 'Review Rating Label: ', 'comments-ratings' ),
-			'default' => esc_html__( 'Your overall rating of this listing:', 'comments-ratings' ),
+			'default' => esc_html__( '이 하숙에 대한 평가점수', 'comments-ratings' ),
 			'type'    => 'text',
 			'size'    => "80"
 		),
 		'review_title_label'       => array(
 			'label'   => esc_html__( 'Review Title Label: ', 'comments-ratings' ),
-			'default' => esc_html__( 'Title of your review', 'comments-ratings' ),
+			'default' => esc_html__( '리뷰 제목:', 'comments-ratings' ),
 			'type'    => 'text',
 			'size'    => "80"
 		),
 		'review_title_placeholder' => array(
 			'label'   => esc_html__( 'Review Title Placeholder: ', 'comments-ratings' ),
-			'default' => esc_html__( 'Summarize your opinion or highlight an interesting detail', 'comments-ratings' ),
+			'default' => esc_html__( '간단하게 리뷰의 내용을 요약해 주세요', 'comments-ratings' ),
 			'type'    => 'text',
 			'size'    => "80"
 		),
 		'review_label'             => array(
 			'label'   => esc_html__( 'Review Label: ', 'comments-ratings' ),
-			'default' => esc_html__( 'Your Review', 'comments-ratings' ),
+			'default' => esc_html__( '상세 리뷰', 'comments-ratings' ),
 			'type'    => 'text',
 			'size'    => "80"
 		),
 		'review_placeholder'       => array(
 			'label'   => esc_html__( 'Review Placeholder: ', 'comments-ratings' ),
-			'default' => esc_html__( 'Tell about your experience or leave a tip for others', 'comments-ratings' ),
+			'default' => esc_html__( '이 하숙에 대한 여러분의 전반적인 평가를 요약해 주세요', 'comments-ratings' ),
 			'type'    => 'text',
 			'size'    => "80"
 		),
 		'review_submit_button'     => array(
 			'label'   => esc_html__( 'Review Submit Button: ', 'comments-ratings' ),
-			'default' => esc_html__( 'Submit your Review', 'comments-ratings' ),
+			'default' => esc_html__( '리뷰 제출', 'comments-ratings' ),
 			'type'    => 'text',
 			'size'    => "80"
 		),
