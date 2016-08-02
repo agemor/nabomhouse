0ed5cad1159a1bbfe8a26de2274e7b83000222f5
HyunJun Kim <me@hyunjun.org>
2016-08-02 21:04:20 +0900

    Add Korean translation to login service

--- web/nabomhouse-theme/plugins/login-with-ajax/modal/widget_out.php
+++ web/nabomhouse-theme/plugins/login-with-ajax/modal/widget_out.php
@@ -1,38 +1,38 @@
-<?php 
+<?php
 /*
- * This is the page users will see logged out. 
+ * This is the page users will see logged out.
  * You can edit this, but for upgrade safety you should copy and modify this file into your template folder.
  * The location from within your template folder is plugins/login-with-ajax/ (create these directories if they don't exist)
 */
 ?>
 	<div class="lwa lwa-template-modal"><?php //class must be here, and if this is a template, class name should be that of template directory ?>
 
-		<?php 
+		<?php
 		//FOOTER - once the page loads, this will be moved automatically to the bottom of the document.
 		?>
 		<div class="lwa-modal" style="display:none;">
 			<form name="lwa-form" class="lwa-form  lwa-login  js-lwa-login  form-visible" action="<?php echo esc_attr(LoginWithAjax::$url_login); ?>" method="post">
 				<p>
-					<label for="username"><?php esc_html_e( 'Username or Email Address','listable' ) ?> *</label>
+					<label for="username"><?php esc_html_e( '아이디 혹은 이메일','listable' ) ?> *</label>
 					<input type="text" name="log" id="lwa_user_login" class="input" />
 				</p>
 				<p>
-					<label for="password"><?php esc_html_e( 'Password','listable' ) ?> *</label>
+					<label for="password"><?php esc_html_e( '비밀번호','listable' ) ?> *</label>
 					<input type="password" name="pwd" id="lwa_user_pass" class="input" value="" />
 				</p>
 				<?php do_action('login_form'); ?>
 				<p class="lwa-meta  grid">
 					<span class="grid__item w50  remember-me">
-						<input name="rememberme" type="checkbox" id="lwa_rememberme" class="remember-me-checkbox" value="1" /><label for="lwa_rememberme"><?php esc_html_e( 'Remember me','listable' ) ?></label>
+						<input name="rememberme" type="checkbox" id="lwa_rememberme" class="remember-me-checkbox" value="1" /><label for="lwa_rememberme"><?php esc_html_e( '자동 로그인','listable' ) ?></label>
 					</span>
 					<?php if( !empty($lwa_data['remember']) ): ?>
 					<span class="grid__item  w50  lost-password">
-						<a class="lwa-show-remember-pass  lwa-action-link  js-lwa-open-remember-form" href="<?php echo esc_attr(LoginWithAjax::$url_remember); ?>" title="<?php esc_attr_e('Password Lost and Found','listable') ?>"><?php esc_attr_e('Lost your password?','listable') ?></a>
+						<a class="lwa-show-remember-pass  lwa-action-link  js-lwa-open-remember-form" href="<?php echo esc_attr(LoginWithAjax::$url_remember); ?>" title="<?php esc_attr_e('아이디/비밀번호 찾기','listable') ?>"><?php esc_attr_e('아이디/비밀번호 찾기','listable') ?></a>
 					</span>
 					<?php endif; ?>
 				</p>
 				<p class="lwa-submit-wrapper">
-					<button type="submit" name="wp-submit" class="lwa-wp-submit" tabindex="100"><span class="button-arrow"><?php esc_attr_e('Log In','listable'); ?></span></button>
+					<button type="submit" name="wp-submit" class="lwa-wp-submit" tabindex="100"><span class="button-arrow"><?php esc_attr_e('로그인','listable'); ?></span></button>
 					<input type="hidden" name="lwa_profile_link" value="<?php echo !empty($lwa_data['profile_link']) ? 1:0 ?>" />
 					<input type="hidden" name="login-with-ajax" value="login" />
 					<?php if( !empty($lwa_data['redirect']) ): ?>
@@ -41,7 +41,7 @@
 				</p>
 				<?php if ( get_option('users_can_register') && !empty($lwa_data['registration']) ) : ?>
 				<p class="lwa-bottom-text">
-					<?php echo __('Don\'t have an account?', 'listable'); ?> <a href="<?php echo esc_attr(LoginWithAjax::$url_register); ?>" class="lwa-action-link  js-lwa-open-register-form"><?php esc_html_e('Sign up','listable'); ?></a>
+					<?php echo __('아이디가 없다면 가입하세요! ', 'listable'); ?> <a href="<?php echo esc_attr(LoginWithAjax::$url_register); ?>" class="lwa-action-link  js-lwa-open-register-form"><?php esc_html_e('회원 가입','listable'); ?></a>
 				</p>
 				<?php endif; ?>
 			</form>
@@ -49,16 +49,16 @@
         	<?php if( !empty($lwa_data['remember']) ): ?>
 	        <form name="lwa-remember" class="lwa-remember  lwa-form  js-lwa-remember" action="<?php echo esc_attr(LoginWithAjax::$url_remember); ?>" method="post" style="display:none;">
 				<p>
-					<label><?php esc_html_e("Forgotten Password", 'listable'); ?></label>
+					<label><?php esc_html_e("아이디/비밀번호 찾기", 'listable'); ?></label>
 				    <input type="text" name="user_login" id="lwa_user_remember" />
 					<?php do_action('lostpassword_form'); ?>
 				</p>
 				<p class="lwa-submit-wrapper">
-	                <button type="submit"><span class="button-arrow"><?php esc_attr_e("Get New Password", 'listable'); ?></span></button>
+	                <button type="submit"><span class="button-arrow"><?php esc_attr_e("비밀번호 새로 설정하기", 'listable'); ?></span></button>
 	                <input type="hidden" name="login-with-ajax" value="remember" />
 				</p>
 		        <p class="cancel-button-wrapper">
-			        <a href="#" class="lwa-action-link  js-lwa-close-remember-form"><?php esc_html_e("Cancel",'listable'); ?></a>
+			        <a href="#" class="lwa-action-link  js-lwa-close-remember-form"><?php esc_html_e("취소",'listable'); ?></a>
 		        </p>
 
 	        </form>
@@ -66,12 +66,12 @@
 		    <?php if ( get_option('users_can_register') && !empty($lwa_data['registration']) ) : //Taken from wp-login.php ?>
 			<form name="lwa-register" class="lwa-register  lwa-form  js-lwa-register" action="<?php echo esc_attr(LoginWithAjax::$url_register); ?>" method="post">
 				<p>
-					<label for="username"><?php esc_html_e( 'Username','listable' ) ?></label>
+					<label for="username"><?php esc_html_e( '아이디(영문)','listable' ) ?></label>
 					<input type="text" name="user_login" id="user_login" />
 				</p>
 
 				<p>
-					<label for="user_email"><?php esc_html_e( 'Email address','listable' ) ?></label>
+					<label for="user_email"><?php esc_html_e( '이메일','listable' ) ?></label>
 					<input type="text" name="user_email" id="user_email" />
 				</p>
 				<?php
@@ -80,16 +80,16 @@
 				?>
 
 				<p class="lwa-meta">
-					<?php esc_html_e('A password will be e-mailed to you.','listable') ?><br />
+					<?php esc_html_e('입력한 이메일 주소로 비밀번호가 발송되었습니다!','listable') ?><br />
 				</p>
 
 				<p class="lwa-submit-wrapper">
-					<button type="submit" tabindex="100"><span class="button-arrow"><?php esc_attr_e('Register','listable'); ?></span></button>
+					<button type="submit" tabindex="100"><span class="button-arrow"><?php esc_attr_e('회원 가입','listable'); ?></span></button>
 					<input type="hidden" name="login-with-ajax" value="register" />
 				</p>
 
 				<p class="lwa-bottom-text">
-					<?php echo __('Already have an account?', 'listable'); ?> <a href="#" class="lwa-action-link  js-lwa-close-register-form"><?php esc_html_e('Log in', 'listable'); ?></a>
+					<?php echo __('이미 아이디가 있으신가요?', 'listable'); ?> <a href="#" class="lwa-action-link  js-lwa-close-register-form"><?php esc_html_e('로그인', 'listable'); ?></a>
 				</p>
 			</form>
 			<?php endif; ?>
